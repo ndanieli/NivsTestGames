d3f6e3dad58a03132bfc91fdd04c48bc998f4a55
Niv <ndanieli@gmail.com>
2017-07-03 17:46:22 +0300

    Rock Paper Scissors done

--- NivsTestGames/Form1.Designer.cs
+++ NivsTestGames/Form1.Designer.cs
@@ -35,17 +35,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 21);
+            this.label1.Location = new System.Drawing.Point(14, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.Size = new System.Drawing.Size(86, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Available Games";
             // 
             // buttonRPS
             // 
-            this.buttonRPS.Location = new System.Drawing.Point(22, 53);
+            this.buttonRPS.Location = new System.Drawing.Point(16, 43);
+            this.buttonRPS.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonRPS.Name = "buttonRPS";
-            this.buttonRPS.Size = new System.Drawing.Size(168, 28);
+            this.buttonRPS.Size = new System.Drawing.Size(126, 23);
             this.buttonRPS.TabIndex = 1;
             this.buttonRPS.Text = "Rock-Paper-Scissors";
             this.buttonRPS.UseVisualStyleBackColor = true;
@@ -53,13 +55,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(212, 206);
             this.Controls.Add(this.buttonRPS);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Game Selector";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- NivsTestGames/RPSForm.Designer.cs
+++ NivsTestGames/RPSForm.Designer.cs
@@ -54,63 +54,70 @@ namespace NivsTestGames
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 29);
+            this.label1.Location = new System.Drawing.Point(19, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 17);
+            this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Number Of Wins";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(377, 29);
+            this.label2.Location = new System.Drawing.Point(283, 24);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(126, 17);
+            this.label2.Size = new System.Drawing.Size(94, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Number Of Losses";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(197, 29);
+            this.label3.Location = new System.Drawing.Point(148, 24);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.Size = new System.Drawing.Size(91, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Number Of Draws";
             // 
             // labelNumOfWins
             // 
             this.labelNumOfWins.AutoSize = true;
-            this.labelNumOfWins.Location = new System.Drawing.Point(159, 29);
+            this.labelNumOfWins.Location = new System.Drawing.Point(119, 24);
+            this.labelNumOfWins.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelNumOfWins.Name = "labelNumOfWins";
-            this.labelNumOfWins.Size = new System.Drawing.Size(16, 17);
+            this.labelNumOfWins.Size = new System.Drawing.Size(13, 13);
             this.labelNumOfWins.TabIndex = 3;
             this.labelNumOfWins.Text = "0";
             // 
             // labelNumOfLosses
             // 
             this.labelNumOfLosses.AutoSize = true;
-            this.labelNumOfLosses.Location = new System.Drawing.Point(525, 29);
+            this.labelNumOfLosses.Location = new System.Drawing.Point(394, 24);
+            this.labelNumOfLosses.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelNumOfLosses.Name = "labelNumOfLosses";
-            this.labelNumOfLosses.Size = new System.Drawing.Size(16, 17);
+            this.labelNumOfLosses.Size = new System.Drawing.Size(13, 13);
             this.labelNumOfLosses.TabIndex = 4;
             this.labelNumOfLosses.Text = "0";
             // 
             // labelNumOfDraws
             // 
             this.labelNumOfDraws.AutoSize = true;
-            this.labelNumOfDraws.Location = new System.Drawing.Point(339, 29);
+            this.labelNumOfDraws.Location = new System.Drawing.Point(254, 24);
+            this.labelNumOfDraws.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelNumOfDraws.Name = "labelNumOfDraws";
-            this.labelNumOfDraws.Size = new System.Drawing.Size(16, 17);
+            this.labelNumOfDraws.Size = new System.Drawing.Size(13, 13);
             this.labelNumOfDraws.TabIndex = 5;
             this.labelNumOfDraws.Text = "0";
             // 
             // buttonPlay
             // 
             this.buttonPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonPlay.Location = new System.Drawing.Point(188, 309);
+            this.buttonPlay.Location = new System.Drawing.Point(141, 251);
+            this.buttonPlay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonPlay.Name = "buttonPlay";
-            this.buttonPlay.Size = new System.Drawing.Size(202, 69);
+            this.buttonPlay.Size = new System.Drawing.Size(152, 56);
             this.buttonPlay.TabIndex = 9;
             this.buttonPlay.Text = "PLAY!";
             this.buttonPlay.UseVisualStyleBackColor = true;
@@ -119,9 +126,10 @@ namespace NivsTestGames
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(157, 392);
+            this.label4.Location = new System.Drawing.Point(118, 318);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(116, 69);
+            this.label4.Size = new System.Drawing.Size(87, 56);
             this.label4.TabIndex = 10;
             this.label4.Text = "Result:";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -129,9 +137,10 @@ namespace NivsTestGames
             // labelResult
             // 
             this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelResult.Location = new System.Drawing.Point(310, 392);
+            this.labelResult.Location = new System.Drawing.Point(232, 318);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelResult.Name = "labelResult";
-            this.labelResult.Size = new System.Drawing.Size(94, 69);
+            this.labelResult.Size = new System.Drawing.Size(70, 56);
             this.labelResult.TabIndex = 11;
             this.labelResult.Text = "NA";
             this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -139,18 +148,20 @@ namespace NivsTestGames
             // lablel7
             // 
             this.lablel7.AutoSize = true;
-            this.lablel7.Location = new System.Drawing.Point(25, 77);
+            this.lablel7.Location = new System.Drawing.Point(19, 63);
+            this.lablel7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lablel7.Name = "lablel7";
-            this.lablel7.Size = new System.Drawing.Size(123, 17);
+            this.lablel7.Size = new System.Drawing.Size(92, 13);
             this.lablel7.TabIndex = 12;
             this.lablel7.Text = "Number of Games";
             // 
             // labelTotalNumberOfGames
             // 
             this.labelTotalNumberOfGames.AutoSize = true;
-            this.labelTotalNumberOfGames.Location = new System.Drawing.Point(159, 77);
+            this.labelTotalNumberOfGames.Location = new System.Drawing.Point(119, 63);
+            this.labelTotalNumberOfGames.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelTotalNumberOfGames.Name = "labelTotalNumberOfGames";
-            this.labelTotalNumberOfGames.Size = new System.Drawing.Size(16, 17);
+            this.labelTotalNumberOfGames.Size = new System.Drawing.Size(13, 13);
             this.labelTotalNumberOfGames.TabIndex = 13;
             this.labelTotalNumberOfGames.Text = "0";
             // 
@@ -158,16 +169,18 @@ namespace NivsTestGames
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(183, 119);
+            this.label5.Location = new System.Drawing.Point(137, 97);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(244, 29);
+            this.label5.Size = new System.Drawing.Size(191, 25);
             this.label5.TabIndex = 14;
             this.label5.Text = "Choose your destiny";
             // 
             // picturePaper
             // 
             this.picturePaper.Image = global::NivsTestGames.Properties.Resources.Paper;
-            this.picturePaper.Location = new System.Drawing.Point(255, 175);
+            this.picturePaper.Location = new System.Drawing.Point(191, 142);
+            this.picturePaper.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.picturePaper.Name = "picturePaper";
             this.picturePaper.Size = new System.Drawing.Size(100, 100);
             this.picturePaper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -178,7 +191,8 @@ namespace NivsTestGames
             // pictureScissors
             // 
             this.pictureScissors.Image = global::NivsTestGames.Properties.Resources.Scissors;
-            this.pictureScissors.Location = new System.Drawing.Point(427, 175);
+            this.pictureScissors.Location = new System.Drawing.Point(320, 142);
+            this.pictureScissors.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureScissors.Name = "pictureScissors";
             this.pictureScissors.Size = new System.Drawing.Size(100, 100);
             this.pictureScissors.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -189,7 +203,8 @@ namespace NivsTestGames
             // pictureRock
             // 
             this.pictureRock.Image = global::NivsTestGames.Properties.Resources.Rock;
-            this.pictureRock.Location = new System.Drawing.Point(84, 175);
+            this.pictureRock.Location = new System.Drawing.Point(63, 142);
+            this.pictureRock.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureRock.Name = "pictureRock";
             this.pictureRock.Size = new System.Drawing.Size(100, 100);
             this.pictureRock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -200,9 +215,10 @@ namespace NivsTestGames
             // buttonReset
             // 
             this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonReset.Location = new System.Drawing.Point(12, 421);
+            this.buttonReset.Location = new System.Drawing.Point(9, 342);
+            this.buttonReset.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonReset.Name = "buttonReset";
-            this.buttonReset.Size = new System.Drawing.Size(99, 50);
+            this.buttonReset.Size = new System.Drawing.Size(74, 41);
             this.buttonReset.TabIndex = 15;
             this.buttonReset.Text = "Reset";
             this.buttonReset.UseVisualStyleBackColor = true;
@@ -210,9 +226,9 @@ namespace NivsTestGames
             // 
             // RPSForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(599, 483);
+            this.ClientSize = new System.Drawing.Size(449, 392);
             this.Controls.Add(this.buttonReset);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.labelTotalNumberOfGames);
@@ -229,10 +245,11 @@ namespace NivsTestGames
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "RPSForm";
-            this.Text = "RPSForm";
+            this.Text = "Rock Paper Scissors";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RPSForm_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.picturePaper)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureScissors)).EndInit();
